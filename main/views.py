91ea740cf10516c80c5746d9f848fab282292e89
seoyeon435 <0305psy@naver.com>
2025-04-08 04:15:54 +0900

    2주차 과제

--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def mainpage(request):
+    return render(request, 'main/hw_mainpage.html')
+
+def secondpage(request):
+    return render(request, 'main/hw_secondpage.html')
